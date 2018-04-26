bed0214b3852802e7519df91fefb1e39a105663d
Massimiliano Arione <garakkio@gmail.com>
2018-04-26 11:26:28 +0200

    refactor constructor

--- src/Converter.php
+++ src/Converter.php
@@ -16,29 +16,7 @@ class Converter
         if (null !== $path && false === $data = @file_get_contents($path)) {
             throw new \UnexpectedValueException('Cannot read file.');
         }
-        $crawler = new Crawler($data);
-        $pages = $crawler->filter('body > doc > page');
-        $ws = [];
-        foreach ($pages as $number => $domElement) {
-            if ($number >= $startPage && $number < $startPage + $numOfPages) {
-                foreach ($domElement->childNodes as $word) {
-                    $ws[] = [
-                        'text' => Cleaner::clear($word->nodeValue),
-                        'xmin' => (float) $word->attributes->getNamedItem('xmin')->value,
-                        'ymin' => (float) $word->attributes->getNamedItem('ymin')->value,
-                        'xmax' => (float) $word->attributes->getNamedItem('xmax')->value,
-                        'ymax' => (float) $word->attributes->getNamedItem('ymax')->value,
-                    ];
-                }
-            }
-        }
-        foreach ($ws as $n => $w) {
-            if ($n < 1 || $w['xmin'] < $ws[$n - 1]['xmax']) {
-                $row = new Row();
-                $this->rows[] = $row;
-            }
-            new Word($w['text'], $row, $w['xmin'], $w['xmax'], $w['ymin'], $w['ymax']);
-        }
+        $this->convert($data, $startPage, $numOfPages);
     }
 
     public function getAsText(): string
@@ -59,5 +37,32 @@ class Converter
         }
 
         return $output;
+    }
+    
+    private function convert(string $data, int $startPage, int $numOfPages): void
+    {
+        $crawler = new Crawler($data);
+        $pages = $crawler->filter('body > doc > page');
+        $ws = [];
+        foreach ($pages as $number => $domElement) {
+            if ($number >= $startPage && $number < $startPage + $numOfPages) {
+                foreach ($domElement->childNodes as $word) {
+                    $ws[] = [
+                        'text' => Cleaner::clear($word->nodeValue),
+                        'xmin' => (float)$word->attributes->getNamedItem('xmin')->value,
+                        'ymin' => (float)$word->attributes->getNamedItem('ymin')->value,
+                        'xmax' => (float)$word->attributes->getNamedItem('xmax')->value,
+                        'ymax' => (float)$word->attributes->getNamedItem('ymax')->value,
+                    ];
+                }
+            }
+        }
+        foreach ($ws as $n => $w) {
+            if ($n < 1 || $w['xmin'] < $ws[$n - 1]['xmax']) {
+                $row = new Row();
+                $this->rows[] = $row;
+            }
+            new Word($w['text'], $row, $w['xmin'], $w['xmax'], $w['ymin'], $w['ymax']);
+        }
     }
 }
